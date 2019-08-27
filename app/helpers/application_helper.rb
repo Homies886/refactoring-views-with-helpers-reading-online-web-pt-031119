0dd2c549eb9c17e70187fd8683e28c23beb4df64
Nicholas Rodriguez <niko876@hotmail.com>
2019-08-27 16:06:43 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def last_updated(post)
-    post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  def title(text)
+    content_for :title, text
   end
 
 end
